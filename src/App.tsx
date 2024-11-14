62bc04a920ec183473822ed5096550dfcc446099
phuongn1810 <phuonglinhn9@gmail.com>
2024-11-14 15:52:57 -0500

    initial landing-page commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Routes, Route} from "react-router";
+import { HashRouter, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import LandingPage from "./LandingPage";
+
+export default function App() {
+ return (
+  <div>
+    <LandingPage />
+   
+  </div>
+);}
